7fc1f4e34322103ab6143683c715f6ff4e9cb059
901184eh <edwin.hurst.q1184@student.hogent.be>
2012-10-25 11:28:13 +0200

    Member implementeert 'Comparable' zodat de vinden, updaten, verwijderen en toevoegen van Members vlotter zou verlopen. equals() en hashCode() zijn aangepast.

--- src/java/logic/Member.java
+++ src/java/logic/Member.java
@@ -9,7 +9,7 @@ package logic;
  * @author Braeburn
  * Represents a client connected to the network.
  */
-public class Member {
+public class Member implements Comparable<Member> {
     private String computerName;
     private String accountName;
     private String softwareVersion;
@@ -17,7 +17,54 @@ public class Member {
     private final String macAddress;
     private String ipAddress;
     
+    /**
+     *
+     * @param macAddress
+     */
     public Member(String macAddress){
         this.macAddress = macAddress;
+    }
+    
+    /**
+     *
+     * @return
+     */
+    public String getMacAddress(){
+        return macAddress;
+    }
+
+    /**
+     *
+     * @param t
+     * @return
+     */
+    @Override
+    public int compareTo(Member t) {
+        return macAddress.compareTo(t.getMacAddress());
+    }
+    
+    /**
+     *
+     * @param t
+     * @return
+     */
+    @Override
+    public boolean equals(Object t){
+        if(!(t instanceof Member)){
+            return false;
+        }
+        Member m = (Member) t;
+        return macAddress.equals(m.getMacAddress());
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 11 * hash + (this.macAddress != null ? this.macAddress.hashCode() : 0);
+        return hash;
     }
 }
